faf2d590385aae83d8900b20702a6f67bd7293fe
Rohan Prinja <prinja@google.com>
2019-04-11 18:18:58 -0700

    schlick approximation for angle-varying reflectivity, and add a bubble to scene

--- material.h
+++ material.h
@@ -36,6 +36,12 @@ bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted) {
   }
 }
 
+float schlick(float cosine, float ref_idx) {
+  float r0 = (1-ref_idx) / (1+ref_idx);
+  r0 *= r0;
+  return r0 + (1-r0)*pow(1-cosine, 5);
+}
+
 class metal : public material {
 public:
   metal(const vec3& a, float f) : albedo(a) { if (f < 1) fuzz = f; else fuzz = 1; }
@@ -58,20 +64,28 @@ public:
     float ni_over_nt;
     attenutation = vec3(1.0, 1.0, 1.0);
     vec3 refracted;
+    float reflect_prob;
+    float cosine;
     if (dot(r_in.direction(), rec.normal) > 0) {
       outward_normal = -rec.normal;
       ni_over_nt = ref_idx;
+      cosine = ref_idx * dot(r_in.direction(), rec.normal) / r_in.direction().length();
     } else {
       outward_normal = rec.normal;
       ni_over_nt = 1.0 / ref_idx;
+      cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
     }
     if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
-      scattered = ray(rec.p, refracted);
-      return true;
+      reflect_prob = schlick(cosine, ref_idx);
     } else {
-      scattered = ray(rec.p, reflected);
-      return false;
+      reflect_prob = 1.0;
     }
+    if (drand48() < reflect_prob) {
+      scattered = ray(rec.p, reflected);
+    } else {
+      scattered = ray(rec.p, refracted);
+    }
+    return true;
   }
 
   float ref_idx;
