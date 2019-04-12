d8b761227611678d3268021bcb147f8bddefbf92
Rohan Prinja <prinja@google.com>
2019-04-12 14:44:30 -0700

    defocus blur and final scene

--- main.cpp
+++ main.cpp
@@ -11,6 +11,46 @@
 #include "util.h"
 #include "vec3.h"
 
+hitable* old_world() {
+  hitable* list[5];
+  list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.1,0.2,0.5)));
+  list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8,0.8,0)));
+  list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8,0.6,0.2), 0.3));
+  list[3] = new sphere(vec3(-1,0,-1), 0.5, new dielectric(1.5));
+  list[4] = new sphere(vec3(-1,0,-1), -0.45, new dielectric(1.5)); // negative radius = bubble
+  return new hitable_list(list, 5);
+}
+
+hitable* random_scene() {
+  int n =500;
+  hitable** list = new hitable*[n+1];
+  list[0] = new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5,0.5,0.5)));
+  int i = 1;
+  for (int a = -11; a < 11; a++) {
+    for (int b = -11; b < 11; b++) {
+      float choose_mat = drand48();
+      vec3 center(a+0.9*drand48(), 0.2, b+0.9*drand48());
+      if ((center-vec3(4,0.2,0)).length() > 0.9) {
+        if (choose_mat < 0.8) { // diffuse
+          list[i++] = new sphere(center, 0.2, new lambertian(vec3(drand48()*drand48(), drand48()*drand48(), drand48()*drand48())));
+        } else if (choose_mat < 0.95) { // metal
+          vec3 albedo(0.5*(1+drand48()), 0.5*(1+drand48()), 0.5*(1+drand48()));
+          float fuzz = 0.5*drand48();
+          list[i++] = new sphere(center, 0.2, new metal(albedo, fuzz));
+        } else { // glass
+          list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+        }
+      }
+    }
+  }
+
+  list[i++] = new sphere(vec3(0,1,0), 1.0, new dielectric(1.5));
+  list[i++] = new sphere(vec3(-4,1,0), 1.0, new lambertian(vec3(0.4,0.2,0.1)));
+  list[i++] = new sphere(vec3(4,1,0), 1.0, new metal(vec3(0.7,0.6,0.5), 0.0));
+
+  return new hitable_list(list, i);
+}
+
 vec3 color(const ray& r, hitable* world, int depth) {
   hit_record rec;
   if (world->hit(r, 0.001, FLT_MAX, rec)) {
@@ -29,26 +69,20 @@ vec3 color(const ray& r, hitable* world, int depth) {
 }
 
 int main() {
-  int nx = 200;
-  int ny = 100;
+  int nx = 1200;
+  int ny = 800;
   int ns = 100;
   std::cout << "P3\n" << nx << " " << ny << " " << "\n255\n";
-  
-  hitable* list[5];
-  list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.1,0.2,0.5)));
-  list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8,0.8,0)));
-  list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8,0.6,0.2), 0.3));
-  list[3] = new sphere(vec3(-1,0,-1), 0.5, new dielectric(1.5));
-  list[4] = new sphere(vec3(-1,0,-1), -0.45, new dielectric(1.5)); // negative radius = bubble
-  hitable* world = new hitable_list(list, 5);
 
-  // hitable* list[2];
-  // float R = cos(M_PI/4);
-  // list[0] = new sphere(vec3(-R,0,-1), R, new lambertian(vec3(0,0,1)));
-  // list[1] = new sphere(vec3(R,0,-1), R, new lambertian(vec3(1,0,0)));
-  // hitable* world = new hitable_list(list, 2);
+  vec3 lookfrom(13,2,3);
+  vec3 lookat(0,0,0);
+  float dist_to_focus = 10.0;
+  float aperture = 0.1;
+  float vfov = 20.0;
 
-  camera cam(vec3(-2,2,1), vec3(0,0,-1), vec3(0,1,0), 30, float(nx)/float(ny));
+  hitable* world = random_scene();
+
+  camera cam(lookfrom, lookat, vec3(0,1,0), vfov, float(nx)/float(ny), aperture, dist_to_focus);
 
   for (int j = ny-1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
@@ -57,7 +91,6 @@ int main() {
         float u = float(i + drand48()) / float(nx);
         float v = float(j + drand48()) / float(ny);
         ray r = cam.get_ray(u, v);
-        // vec3 p /= r.point_at_parameter(2.0);
         col += color(r, world, 0);
       }
       col /= float(ns);
